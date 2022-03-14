dabbc35c5a3d6ed5633972808c2d5a0607a92248
Jovita <jovitaspais@gmail.com>
2022-03-14 13:44:00 -0400

    animate the clicked the button

--- game.js
+++ game.js
@@ -1,13 +1,37 @@
-const buttonColours = ["red", "blue", "green", "yellow"];
+const buttonColours = ['red', 'blue', 'green', 'yellow'];
 const gamePattern = [];
+const userClickedPattern = [];
+
+$('.btn').click((e) => {
+  
+  let userChosenColour = e.target.id;
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
 
 const nextSequence = () => {
   const randomNumber = Math.floor(Math.random() * 4);
   const randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  $('#' + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
-  const bleep = new Audio("sounds/" + randomChosenColour + ".mp3");
-  bleep.play();
+  // Animate flash using jquery
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
 };
+
+function playSound(name) {
+  const audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+const animatePress = (currentColour) => {
+  $("#" + currentColour).addClass('pressed');
+
+ //use Javascript to remove the pressed class after a 100 milliseconds.
+  setTimeout(() => {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+};
+nextSequence();
+animatePress();
